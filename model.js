bf2ae49e93f3a2212c48bb66c5a981467c8f9a6e
Justin Nietzer <justinnietzer@gmail.com>
2018-07-12 17:43:10 -0400

    added get

--- model.js
+++ model.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => res.send('ok'));
 
 app.listen(process.env.PORT || 8080, () => console.log(
 	`Your app is listening on port ${process.env.PORT || 8080}`)
